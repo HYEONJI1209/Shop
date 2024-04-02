a5e666d54ffae319e37dc4dec3f1d85eac2ff210
kang yoon <rkddbs0813@gmail.com>
2024-04-02 19:22:47 +0900

    DB 연동 실패

---
+++ backend/wildfix/src/main/java/com/shop/wildfix/WildfixApplication.java
@@ -0,0 +1,13 @@
+package com.shop.wildfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildfixApplication.class, args);
+	}
+
+}
