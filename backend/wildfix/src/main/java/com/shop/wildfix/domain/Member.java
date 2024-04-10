28315020e4d8721d224331c328344f2a138e03a7
YoonmaYer <rkddbs0813@gmail.com>
2024-04-10 20:18:59 +0900

    proxy 설정 확인

--- backend/wildfix/src/main/java/com/shop/wildfix/domain/Member.java
+++ backend/wildfix/src/main/java/com/shop/wildfix/domain/Member.java
@@ -13,14 +13,14 @@ public class Member {
 
     private String username;
     private String password;
-    private String name;
+    private String email;
 
     public Member() {}
 
-    public Member(String username, String password, String name) {
+    public Member(String username, String password, String name, String email) {
         this.username = username;
         this.password = password;
-        this.name = name;
+        this.email = email;
     }
 
     // Getter and Setter methods
@@ -48,11 +48,11 @@ public class Member {
         this.password = password;
     }
 
-    public String getName() {
-        return name;
+    public String getEmail() {
+        return email;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
