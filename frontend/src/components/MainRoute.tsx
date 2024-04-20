763ce56ec639f66ba8f046964b1212e060864b81
HYEONJI1209 <108385746+HYEONJI1209@users.noreply.github.com>
2024-04-21 01:32:11 +0900

    오류

--- frontend/src/components/MainRoute.tsx
+++ frontend/src/components/MainRoute.tsx
@@ -1,33 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
-import { Mainpage } from "./index";
+import { Mainpage, Register } from "./index";
 import { Routes, Route } from "react-router-dom";
 
-interface RouteProps {
-=======
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
-import {Register} from "./index";
-
-const MainRoute = () => {
-    return (
-        <div>
-            <Routes>
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </div>
-    )
->>>>>>> 6a54f524831f9b4c1275c1134efe2649e46dcbe9
-}
+interface RouteProps {}
 
 const MainRoute: React.FC<RouteProps> = ({ }) => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Mainpage />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </div>
     );
